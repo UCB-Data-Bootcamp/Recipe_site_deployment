13098b194174f8b460370af07ebf56909afbb1d3
surfaki <143300729+surfaki@users.noreply.github.com>
2024-02-21 21:34:29 -0800

    Update load_table.py

--- load_table.py
+++ load_table.py
@@ -4,6 +4,6 @@ from pathlib import Path
 def load_table():
 
     #load files
-    df_ingredient=pd.read_csv('../ingredient_dummy_short2.csv')
-    df_keyword=pd.read_csv('../keyword_dummy_short2.csv')
+    df_ingredient=pd.read_csv('./ingredient_dummy_short2.csv')
+    df_keyword=pd.read_csv('./keyword_dummy_short2.csv')
     return df_ingredient.to_dict(), df_keyword.to_dict()
