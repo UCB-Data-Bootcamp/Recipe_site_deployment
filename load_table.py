9723572e2e0ce811e013f3abb2147b3791bb2c1b
surfaki <143300729+surfaki@users.noreply.github.com>
2024-02-21 22:51:59 -0800

    Update load_table.py

--- load_table.py
+++ load_table.py
@@ -6,4 +6,5 @@ def load_table():
     #load files
     df_ingredient=pd.read_csv('./ingredient_dummy_short2.csv')
     df_keyword=pd.read_csv('./keyword_dummy_short2.csv')
-    return df_ingredient.to_dict(), df_keyword.to_dict()
+    # return df_ingredient.to_dict(), df_keyword.to_dict()
+    return 1
