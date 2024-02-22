e8375acfede54d36bf5d2e324a72792d03c994d6
surfaki <143300729+surfaki@users.noreply.github.com>
2024-02-22 00:32:15 -0800

    Update flask_init.py

--- flask_init.py
+++ flask_init.py
@@ -17,7 +17,7 @@ def home():
     # print(json.dumps(ML_analysis_based_on_web_input.top_ingredients(df_ingredient, df_keyword)))
     # print(json.dumps(df_ingredient[1]))
     # print(jsonify(df_ingredient[1]))
-    print(jsonify(15))
+    print(json.dumps(15))
     return json.dumps(13)
     
     
